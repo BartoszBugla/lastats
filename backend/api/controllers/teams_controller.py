5e589a92471db455193feb2a133af7f146fb242f
Bartek Pacia <barpac02@gmail.com>
2023-06-12 23:14:29 +0200

    create simple `teams_controller`, delete some "test" controllers

---
+++ backend/api/controllers/teams_controller.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, Response, jsonify, request
+
+
+teams_bp = Blueprint("teams", __name__)
+
+teams = {"teams": [
+        {"id": 1, "name": "Team 1"},
+        {"id": 2, "name": "Team XD"},
+    ]}
+
+@teams_bp.get("/")
+def get_teams():
+    return teams
+
+@teams_bp.post("/")
+def add_team():
+    team = {
+        'name': request.form.get("name"),
+        'id': len(teams["teams"]) + 1
+    }
+
+    teams["teams"].append(team)
+
+    return Response(status=201)
+
+
+@teams_bp.put("/<int:id>")
+def update_team(id):
+    team = teams["teams"][id]
+    team["name"] = request.form.get("name")
+
+    return Response(status=200)
+
+@teams_bp.delete("/<int:id>")
+def delete_team(id):
+    teams["teams"].pop(id)
+
+    return Response(status=200)
