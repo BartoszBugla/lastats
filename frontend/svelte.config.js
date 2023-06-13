3bfe2eb0a7e9e63c0f7bcf4476def8637069c003
BartoszBugla <48729791+BartoszBugla@users.noreply.github.com>
2023-06-13 22:49:15 +0200

    base gh action

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,11 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-/** @type {import('@sveltejs/kit').Config} */
 
+const dev = process.argv.includes('dev');
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
-  },
+    adapter: adapter(),
+    paths: {
+        base: dev ? '' : process.env.BASE_PATH,
+    }
+},
   preprocess: vitePreprocess()
 };
 
